1b1567c5d6e6bfb81cbe56e73881585033629831
Jesus Alberto <albto02@gmail.com>
2024-03-09 01:14:29 -0600

    Variables informacion

---
+++ clase5-anatomiaDUVariable.js
@@ -0,0 +1,24 @@
+let cajaDeAndy = 'Woody'
+console.log(cajaDeAndy)
+
+// Lo NO permitido
+let c = 'Woody'
+//Evitar abreviaciones y escribir completas las palabras
+let cda = 'Woody'
+let pcAndy = 'Woody'
+let c, cda
+    pcAndy
+
+// Lo permitido
+let primerTrasteoDeAndy = 'Woody';
+let urlDelUsuario = 'https://www.google.com'
+let idDelUsuario = '123456789'
+//Variables siempre esten al inicio de el codigo.
+//En orden y una debajo de la otra.
+
+//Let para crear variables va a cambiar
+let contador
+contador = contador + 1
+
+//const la usamos para variables constantes o fijas, que no se van a modificar
+const pi = 3.14
